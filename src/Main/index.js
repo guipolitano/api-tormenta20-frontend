57e02cafe36a4dbeaf0ee87b793ba836c47cecef
Guilherme Politano <gui_politano@hotmail.com>
2019-10-25 08:34:29 -0300

    Finaliza card, adiciona input e label"

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import ReactJson from 'react-json-view'
- 
-import { Container, Card } from './styles';
-import NavBar from '../NavBar';
+import React from "react";
+import ReactJson from "react-json-view";
+
+import { Container, Card, Input, Label } from "./styles";
+import NavBar from "../NavBar";
 
 export default function Main() {
-    const data = {
-        "1":{
-            "teste": "teste"
-        }
+  const data = {
+    "1": {
+      teste: "teste"
     }
+  };
   return (
     <>
-        <NavBar/>
-        <Container>
-            <Card>
-                teste
-            </Card>
-            <Card>
-            <ReactJson src={data} theme="monokai" iconStyle="circle" displayDataTypes={false}/> 
-            </Card>
-        </Container>
+      <NavBar />
+      <Container>
+        <Card>
+          <Label>Insira o que deseja procurar</Label>
+          <Input />
+        </Card>
+        <Card>
+          <ReactJson
+            src={data}
+            theme="monokai"
+            iconStyle="circle"
+            displayDataTypes={false}
+          />
+        </Card>
+      </Container>
     </>
   );
 }
--- src/Main/styles.js
+++ src/Main/styles.js
@@ -1,5 +1,5 @@
-import styled from 'styled-components';
-import background from '../assets/img/background.jpg';
+import styled from "styled-components";
+import background from "../assets/img/background.jpg";
 
 export const Container = styled.div`
   display: flex;
@@ -7,22 +7,38 @@ export const Container = styled.div`
   justify-content: center;
   align-items: center;
   background-image: url(${background});
-  height:100%;
+  height: 100%;
   width: 100%;
 `;
 
 export const Card = styled.div`
   display: flex;
-  min-width: 50%;
-  @media (min-width:300px) {
-      min-width: 90%;
+  width: 50%;
+  @media (max-width: 1200px) {
+    width: 60%;
+  }
+  @media (max-width: 800px) {
+    width: 80%;
   }
   padding: 10px 10px;
   justify-content: space-between;
   flex-wrap: wrap;
   flex-direction: column;
-  background: grey;
+  background: #272822;
   border: 5px ridge orange;
   border-radius: 5px;
   flex: 0 1 calc(25% -1em);
+  margin: 2px;
+  transition: all 0.5s ease-in-out;
+`;
+
+export const Input = styled.input`
+  background-color: white;
+  border-radius: 5px;
+  height: 20px;
+  padding: 5px;
+`;
+
+export const Label = styled.label`
+  color: white;
 `;
